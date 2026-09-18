0977506178a2c1199b4f2da646720262462960df
Lewis Chavara <lewischavara948@gmail.com>
2026-09-18 19:31:22 +0100

    removed unused code

--- bots/bot8.py
+++ bots/bot8.py
@@ -114,8 +114,6 @@ def level8(board):
         -50, -40, -30, -20, -20, -30, -40, -50
     ]
     
-    score = 0
-
     #black piece-square tables
     pawn_black = [
         0, 0, 0, 0, 0, 0, 0, 0,
