9db05020968599bc036c782f9cd5df8f6a5da1e6
Lewis Chavara <lewischavara948@gmail.com>
2026-09-18 16:33:06 +0100

    fixed spelling

--- bots/bot4.py
+++ bots/bot4.py
@@ -291,7 +291,7 @@ def level4(board):
                 if alpha >= beta:
                     break
         else:
-            #playing as black: minimize evaluation (black pieces are negative)
+            #playing as black: minimise evaluation (black pieces are negative)
             best_value = float("inf")
             for move in board.legal_moves:
                 board_copy = board.copy()
