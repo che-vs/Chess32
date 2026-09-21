70eebccd830ac8f41095d43b63c886b230f9296f
Lewis Chavara <lewischavara948@gmail.com>
2026-09-21 20:06:38 +0100

    increased transposition table size

--- main/bots/bot8.py
+++ main/bots/bot8.py
@@ -524,7 +524,7 @@ def level8(board):
         time_lim = 15
         start_time = time.time()
         
-        if len(transposition_table) > 50000: #clears every 50000 items
+        if len(transposition_table) > 500000: #clears every 500000 items
             transposition_table.clear()
                     
         best_move_found = None
